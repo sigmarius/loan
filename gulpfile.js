86cd4b9308579413eb708c439b2fa1474b270a47
Anastasya <aamokeeva@yandex.ru>
2021-07-07 19:15:30 +0300

    Начинает вести историю

---
+++ gulpfile.js
@@ -0,0 +1,115 @@
+"use strict";
+
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const browsersync = require("browser-sync");
+
+const dist = "./dist/";
+// для тестирования на локальном сервере
+// const dist  = "C:/MAMP/htdocs/test";
+
+const ghPages = require('gh-pages');
+const path = require('path');
+
+gulp.task("copy-html", () => {
+    return gulp.src("./src/*.html")
+                .pipe(gulp.dest(dist))
+                .pipe(browsersync.stream());
+});
+
+gulp.task("copy-readme", () => {
+    return gulp.src("README.md")
+                .pipe(gulp.dest(dist));
+});
+
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist))
+                .on("end", browsersync.reload);
+});
+
+gulp.task("copy-assets", () => {
+    return gulp.src("./src/assets/**/*.*")
+                .pipe(gulp.dest(dist + "/assets"))
+                .on("end", browsersync.reload);
+});
+
+gulp.task("watch", () => {
+    browsersync.init({
+        server: {
+            baseDir: "./dist/",
+            serveStaticOptions: {
+                extensions: ["html"]
+            }
+        },
+		port: 4000,
+		notify: true
+    });
+    
+    gulp.watch("./src/*.html", gulp.parallel("copy-html"));
+    gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
+    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+});
+
+gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js"));
+
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'production',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist));
+});
+
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), './dist'), cb);
+}
+exports.deploy = deploy;
+
+gulp.task("default", gulp.parallel("watch", "build"));
