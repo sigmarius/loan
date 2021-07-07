d855c97320ac671d40ae034929b2f8e493a33e12
Anastasya <aamokeeva@yandex.ru>
2021-07-07 21:54:15 +0300

    Реализует показ видео в модальном окне

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 import Slider from "./modules/slider";
+import Video from "./modules/video";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const player = new Video('.showup .play', '.overlay');
+    player.init();
 });
+
